64330c30bda18341f172aaeaa43335b1eba6ac1f
sujeethcodes <sujeethcodes@gmail.com>
2025-05-17 17:36:36 +0530

    forloop-control

--- controler-structure/for_loop.go
+++ controler-structure/for_loop.go
@@ -1,1 +1,20 @@
 package main
+
+import "fmt"
+
+// This program demonstrates how to use the `for` loop in Golang.
+// The loop runs from 1 to 10 and checks whether each number is even or odd.
+// The format of a basic for loop in Go:
+// for initialization; condition; increment {}
+
+func main() {
+	// check the even number
+	n := 10
+	for i := 1; i <= n; i++ {
+		if i%2 == 0 {
+			fmt.Println("Even")
+		} else {
+			fmt.Println("odd")
+		}
+	}
+}
